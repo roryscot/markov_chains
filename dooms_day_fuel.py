91d526ff116f986bd2590f672036671b916357a2
Rory Dahl <rsdah13@gmail.com>
2017-04-18 20:30:44 -0400

    1

--- dooms_day_fuel.py
+++ dooms_day_fuel.py
@@ -1,10 +1,232 @@
 from fractions import Fraction
 
-#retry with integer probability matrix
-#try to maintain fractions throughout
-      #.limit_denominator
-#try reverting back to using denominator but with identity_matrix
+def answer(m):
+  result = []
 
+
+  print("Original Matrix: ")
+  for i in m:
+    print(i)
+  print(" ")
+
+
+ #----Initial Info------
+  terminals = terminal_finder(m)
+  divisions = divisions_of(m)
+  common_denominator = common_denominator_finder(divisions)
+  fractions = fratctionator(m, divisions)
+
+  mapped_order = mapper(m,terminals)
+  reference_order = find_reference_order(mapped_order)
+
+  #New row order
+  row_order = reordered_rows(mapped_order)
+  #Change columns
+  columns_order = column_sorter(m,row_order,reference_order)
+  #Move new columns to new rows
+  working_order = make_working_order(columns_order,reference_order)
+  #Reorder divisions
+
+
+  #-------Probability Matrix
+  probabilities = probability_matrix(working_order, divisions, common_denominator, terminals)
+
+  #print(terminals)
+  print(divisions)
+  print(common_denominator)
+  #print(mapped_order)
+  #print(working_order)
+  #print(fractions)
+
+
+
+
+  return result
+
+
+
+
+
+
+ #======Initial Info ===================
+
+def terminal_finder(m):
+  terminal = []
+
+  for i in range(0,len(m)):
+    sum = 0
+    for j in range(0,len(m[i])):
+      sum += m[i][j]
+    if sum == 0 or (m[i][i] == sum) and (i not in terminal):
+      terminal.append(i)
+
+  for i in range(1,len(m)):
+    default_terminal = 0
+    j = 0
+    if i not in terminal:
+      if m[i][j] == 0:
+        default_terminal += 1
+      if default_terminal == len(m):
+        terminal.append(i)
+    j+=1
+
+  return terminal
+
+          ##########
+
+def divisions_of(m):
+  divisions = []
+
+  for i in range(0,len(m)):
+    sum = 0
+    for j in range(0,len(m[i])):
+      sum += m[i][j]
+    if sum == 0:
+      divisions.append(1)
+    else:
+      divisions.append(sum)
+  return divisions
+
+          ##########
+
+def common_denominator_finder(array):
+  common_denominator = 1
+  for i in array:
+    if i != 0:
+        common_denominator *= i
+  return common_denominator
+
+
+          ##########
+
+
+def mapper(m,terminal):
+  mapp = []
+  for i in range(len(m)):
+    if i not in terminal:
+      mapp.append([i,m[i]])
+  for i in range(len(m)):
+    if i in terminal:
+      mapp.append([i,m[i]])
+
+  return mapp
+
+          ##########
+
+def reordered_rows(mapped_order):
+  # print("reordered_matrix: ")
+  working_order = []
+  for i in range(len(mapped_order)):
+    working_order.append(mapped_order[i][1])
+    # print(working_order[i])
+  # print(" ")
+  return working_order
+
+          ##########
+
+def fratctionator(m, divisions):
+  fractions = []
+  for i in range(0,len(m)):
+    for j in range(0,len(m[i])):
+      if m[i][j] != 0:
+        fractions.append([m[i][j],divisions[i]])
+  return fractions
+
+         ##########
+
+def column_sorter(m,m_prime,order):
+  #this depends on the number of terminals being right
+  print("columns switched")
+  new = []
+  for i in range(len(m)):
+    row = []
+    for j in range(len(m)):
+      row.append(m[i][order[j]])
+      #print(row)
+    new.append(row)
+    print(new[i])
+  return new
+
+          ##########
+
+def find_reference_order(mapp):
+  new = []
+  # print("new order: ")
+  for i in range(len(mapp)):
+    new.append(mapp[i][0])
+    # print(i)
+  # print(" ")
+  return new
+
+          ##########
+
+def make_working_order(m,order):
+  print("working_orderer: ")
+  working_orderer = []
+  for i in range(len(order)):
+    working_orderer.append(m[order[i]])
+    print(working_orderer[i])
+  return working_orderer
+
+
+          ##########
+
+
+
+
+#======Probability Matrix===================
+      #This should examine the reordered matrix so that it give a standard form
+
+def probability_matrix(m, divisions, denominator,terminal):
+  print("probability_matrix: ")
+  ###These values are altered, which changes the mapped_order as well
+  for i in range(0,len(m)):
+    for j in range(0,len(m[i])):
+      if m[i][j] != 0:
+        m[i][j] = (m[i][j])/float(divisions[i])
+      if (m[i][j] == m[i][i]):
+        if i in terminal:
+          m[i][i]=1
+
+    print(m[i])
+  print(" ")
+  return m
+
+#============Matrix Quadrants =================
+
+def q_finder(m, terminal):
+  q = []
+  t = len(m)-len(terminal)
+  for i in range(0,t):
+    row = []
+    for j in range(0,t):
+      row.append(m[i][j])
+    q.append(row)
+    print(row)
+  return q
+
+def q_checker(m):
+  sum = 0
+  for i in range(len(m)):
+    for j in range(len(m)):
+      sum += m[i][j]
+  return sum
+
+
+          ##########
+
+
+def r_finder(m,terminal):
+  r = []
+  t = len(m)-len(terminal)
+  for i in range(0,t):
+    row = []
+    for j in range(t,len(m)):
+      row.append(m[i][j])
+    r.append(row)
+  return r
+
+#==========Matrix Inversion======================
 def transposeMatrix(m):
     t = []
     for r in range(len(m)):
@@ -18,6 +240,9 @@ def transposeMatrix(m):
         print(tRow)
     return t
 
+          ##########
+
+
 def getMatrixMinor(m,i,j):
     return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
 
@@ -30,6 +255,10 @@ def getMatrixDeternminant(m):
     for c in range(len(m)):
         determinant += ((-1)**c)*m[0][c]*getMatrixDeternminant(getMatrixMinor(m,0,c))
     return determinant
+
+
+          ##########
+
 
 def getMatrixInverse(m):
     determinant = getMatrixDeternminant(m)
@@ -55,279 +284,8 @@ def getMatrixInverse(m):
     return cofactors
 
 
-def answer(m):
-    divisions = []
-    numerators = []
-    result = []
-    terminal = []
-    too_many_results = []
 
-
-
-    for i in range(0,len(m)):
-      sum = 0
-      count = 0
-      print(m[i])
-      too_many_results.append(0)
-      for j in range(0,len(m[i])):
-        sum += m[i][j]
-      # print(m[i][i] == sum)
-      # print("This is crazy")
-      # print([m[i][i],sum])
-
-      if sum == 0 or (m[i][i] == sum) and (i not in terminal):
-        terminal.append(i)
-      if sum == 0:
-        divisions.append(1)
-      else:
-        divisions.append(sum)
-
-    for i in range(1,len(m)):
-      default_terminal = 0
-      j = 0
-      if i not in terminal:
-        if m[i][j] == 0:
-          default_terminal += 1
-        if default_terminal == len(m):
-          terminal.append(i)
-      j+=1
-
-
-
-    fractions = fratctionator(m, divisions)
-
-    common_denominator = 1
-    for i in divisions:
-      if i != 0:
-          common_denominator *= i
-
-
-    for i in range(0, len(fractions)):
-      numerators.append(fractions[i][0]*common_denominator/fractions[i][1])
-
-    print(" ")
-
-    print("terminal: ")
-    print(terminal)
-    print(" ")
-
-    print("divisions: ")
-    print(divisions)
-    print(" ")
-
-    print("numerators")
-    print(numerators)
-    print(" ")
-
-    print("common_denominator: ")
-    print(common_denominator)
-    print(" ")
-
-    print("fractions:")
-    print(fractions)
-    print(" ")
-
-    print("probability_matrix:")
-    probs = probability_matrix(m,divisions,common_denominator,terminal)
-    print(" ")
-
-    print("q = ")
-    q = q_finder(probs, terminal)
-    idm = identity_matrix(m,q)
-    print("IDM")
-    print(idm)
-    print(q == idm)
-    print("q checker")
-    print(q_checker(q))
-    print(" ")
-    if q_checker(q)==0 or q == idm:
-      result = matrix_fractionator(m)
-      print(len(result))
-      print(len(terminal))
-      print("FRACTIONATOR")
-      print result
-      if q == idm:
-        return result[len(result)-(len(terminal)+2):]
-      else:
-        return result[len(result)-(len(terminal)+1):]
-
-    print(" ")
-    print("r = ")
-    r = r_finder(probs, terminal)
-    for row in r:
-      print(row)
-
-
-    print(" ")
-    i_q = matrix_subtraction(identity_matrix(m,q), q)
-    print("I-Q")
-    for i in i_q:
-      print(i)
-    #
-    # placeholder
-    #
-    if getMatrixDeternminant(i_q)==0:
-      # numerators.append(common_denominator)
-      # return (numerators)
-
-      print("HEERRERERERE!!!!!!!!!!!!!!!")
-
-
-      result = matrix_fractionator(m)
-      print("Result = ")
-      print(result)
-
-
-      if q == idm:
-        return result[len(result)-(len(terminal)+2):]
-      else:
-        return result[len(result)-(len(terminal)+1):]
-
-
-
-      print("Result = ")
-      print(result)
-
-      result.append(1)
-      return result[len(result)-(len(terminal)+1):]
-
-
-    inverse = getMatrixInverse(i_q)
-    print(" ")
-    print("I - Q inverted")
-    for i in inverse:
-      print(i)
-
-    print(" ")
-    print("(I - Q)^-1 (R): ")
-    #order matters in matrix_multiplier
-    answer_matrix = matrix_multiplier(inverse,r)
-
-    result = matrix_fractionator(matrix_multiplier(inverse,r))
-
-
-    print(" ")
-    print("Result: ")
-    return result
-
-def q_finder(m, terminal):
-  q = []
-  t = len(m)-len(terminal)
-  for i in range(0,t):
-    row = []
-    for j in range(0,t):
-      row.append(m[i][j])
-    q.append(row)
-    print(row)
-  return q
-
-def q_checker(m):
-  sum = 0
-  for i in range(len(m)):
-    for j in range(len(m)):
-      sum += m[i][j]
-  return sum
-
-
-
-def r_finder(m,terminal):
-  r = []
-  t = len(m)-len(terminal)
-  for i in range(0,t):
-    row = []
-    for j in range(t,len(m)):
-      row.append(m[i][j])
-    r.append(row)
-  return r
-
-
-def fratctionator(m, divisions):
-  fractions = []
-  for i in range(0,len(m)):
-    for j in range(0,len(m[i])):
-      if m[i][j] != 0:
-        fractions.append([m[i][j],divisions[i]])
-  return fractions
-
-def probability_matrix(m, divisions, denominator,terminal):
-  for i in range(0,len(m)):
-    for j in range(0,len(m[i])):
-      if m[i][j] != 0:
-        m[i][j] = (m[i][j])/float(divisions[i])
-      if (m[i][j] == m[i][i]):
-        if i in terminal:
-          m[i][i]=1
-
-    print(m[i])
-  return m
-
-def identity_matrix(m,q):
-  identity = []
-  for i in range(0,len(q)):
-    row = []
-    for j in range(0,len(q)):
-      if (i == j ):
-        row.append(1)
-      else:
-        row.append(0)
-    identity.append(row)
-
-    print("identity")
-    print(row)
-  print("identity[0]")
-  identity[0][0]=float(identity[0][0])
-  print(identity[0][0])
-
-  return identity
-
-def matrix_subtraction(identity,m):
-  result = []
-  for i in range(0,len(m)):
-    row = []
-    for j in range(0,len(m)):
-      #print(m[i][j])
-      row.append(identity[i][j]-m[i][j])
-    result.append(row)
-    #print(row)
-  return result
-
-
-def matrix_multiplier(a,b):
-  result = []
-  for i in range(0,len(a)):
-    row = []
-    for j in range(0, len(b[0])):
-      row.append(0)
-    result.append(row)
-
-  for i in range(len(a)):
-   for j in range(len(b[0])):
-       for k in range(len(b)):
-           result[i][j] += a[i][k] * b[k][j]
-
-  for i in result:
-    print(i)
-
-  return result
-
-def matrix_fractionator(m):
-  pairs= []
-  denominators=[]
-  result=[]
-
-  for j in m[0]:
-    pairs.append([Fraction(j).limit_denominator().numerator,Fraction(j).limit_denominator().denominator])
-    denominators.append(Fraction(j).limit_denominator().denominator)
-  greatest_cd=max(denominators)
-
-  for i in pairs:
-    if greatest_cd%i[1] == 0:
-      if greatest_cd!=i[1]:
-        i[0]*=(greatest_cd/i[1])
-    result.append(i[0])
-  result.append(greatest_cd)
-  return(result)
-
+#------============================================--------------
 m = [
   [0, 2, 1, 0, 0],
   [0, 0, 0, 3, 4],
@@ -440,12 +398,12 @@ mikes=[
 
 
 
-print(answer(m))
+#print(answer(m))
 #print(answer(n))
 #print(answer(o))
 #print(answer(testtttt4))
 #print(answer(testtttt10))
-#print(answer(mikes))
+print(answer(mikes))
 
 #example = [[1,0,2,3],[0,1,1,1],[0,0,0,0],[0,0,0,0]]
 
